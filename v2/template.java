02c89f4784204466865d0888f57ee84adf5fc4a0
Austin Leung <aleung5@stuy.edu>
2017-12-04 16:06:18 -0500

    directory

---
+++ v2/template.java
@@ -0,0 +1,17 @@
+/** 
+ *  
+ *  
+ */
+
+public class template {
+
+    /** 
+     *  
+     *  
+     */
+    public static void main(String[] commandLine) {
+        System.out.println( System.lineSeparator()
+                          + "changeMe");
+
+    }
+}
