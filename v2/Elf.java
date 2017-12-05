deb63840353e87140264d74a9d41b545d68dedbd
dtmak1 <34113776+dtmak1@users.noreply.github.com>
2017-12-05 11:35:22 -0500

    adding elf

---
+++ v2/Elf.java
@@ -0,0 +1,11 @@
+public class Elf extends Protagonist {
+
+    public Elf() {
+	hp = 450;
+    }
+
+    // override default values
+    public final static double ATTACK_RATING = 0.75;
+    public final static int DEFENSE_STAT = 20;
+    
+}
