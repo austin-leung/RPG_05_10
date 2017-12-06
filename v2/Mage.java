8f63291e4c8760dfbff9927fa000fbe5bf600673
Sophia Azaraev <sazaraev@stuy.edu>
2017-12-05 20:19:57 -0500

    commit mage1

---
+++ v2/Mage.java
@@ -0,0 +1,9 @@
+public class Mage extends Protagonist {
+    public Mage (String name ){
+        super(name);
+        hp = 100;
+            
+    }
+    public final static double DEFENSE_STAT = 55; 
+        public final static double ATTACK_RATING = 0.3;
+}
